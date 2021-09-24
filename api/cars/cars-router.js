d139e7d19e00df02f8366f23e11c97b3f2b11d51
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-23 22:21:35 -0700

    first get request complete to get all of the cars

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,15 @@
 const express = require('express')
-
+const Car = require('./cars-model')
 const router = express.Router()
 
 
 router.get('/', async (req, res, next) => {
-  res.json('getting all cars')
+  try {
+    const cars = await Car.getAll()
+    res.json('getting all cars')
+  } catch (err){
+    next(err)
+  }
 })
 
 router.get('/:id', async (req, res, next) => {
