01730fa45d679ca641e1123fed3abe72799e4178
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-23 23:28:45 -0700

    fixed errors in seed file, working on router and middleware files

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,21 +1,38 @@
-const express = require('express')
-const Car = require('./cars-model')
-const router = express.Router()
+const express = require("express");
+const Car = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
 
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const cars = await Car.getAll()
-    res.json('getting all cars')
-  } catch (err){
-    next(err)
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-router.get('/:id', async (req, res, next) => {
-  res.json(`getting car with id ${req.params.id}`)
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id);
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json('posting new car')
-})
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    
+  }
+);
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,29 +1,28 @@
 // STRETCH
 const cars = [
   {
-    vin: '1111111111111',
-    make: 'toyota',
-    model: 'prius',
-    milage: 215000,
-    title: 'clean',
-    transmission: 'manual'
+    vin: "2HGFA16559H312891",
+    make: "toyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "manual",
   },
   {
-    vin: '2222222222222',
-    make: 'toyota',
-    model: 'corolla',
-    milage: 115000,
-    title: 'salvage',
-  
+    vin: "KL1TD56E39B357232",
+    make: "toyota",
+    model: "corolla",
+    mileage: 115000,
+    title: "salvage",
   },
   {
-    vin: '3333333333333',
-    make: 'ford',
-    model: 'focus',
-    milage: 15000,
-  }
-]
+    vin: "1G6DM56T550237792",
+    make: "ford",
+    model: "focus",
+    mileage: 15000,
+  },
+];
 exports.seed = async function (knex) {
-  await knex('cars').truncate()
-  await knex('cars').insert(cars)
-}
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
